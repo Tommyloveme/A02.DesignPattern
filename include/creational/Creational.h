a8800be8deefe026a1eaa347a8a4657de214e754
duyonglu <1534780881@qq.com>
2024-03-03 23:57:40 +0800

    [stash]设计模式-创建型模式:Maze

--- include/creational/Creational.h
+++ include/creational/Creational.h
@@ -1,14 +1,78 @@
 #ifndef CREATIONAL_HEADER
 #define CREATIONAL_HEADER
 
+#include<vector>
+
 namespace DP {
 
-class Creational
-{
-public:
-    Creational() {};
-    ~Creational() {};
+enum Direction {
+    North = 0,
+    South,
+    East,
+    West
 };
+
+class MapSite {
+public:
+    virtual void Enter() = 0;
+};
+
+class Room : public MapSite {
+public:
+    Room(int roomNo);
+
+    MapSite* GetSide(Direction) const;
+    void SetSide(Direction, MapSite*);
+
+    virtual void Enter();
+
+protected:
+    MapSite* m_mapSite[4];
+    int m_roomNo;
+};
+
+class Wall : public MapSite {
+public:
+    Wall();
+
+    virtual void Enter();
+};
+
+class Door : public MapSite {
+public:
+    Door(Room* = 0, Room* = 0);
+
+    virtual void Enter();
+
+    Room* OtherSideFrom(Room*);
+
+protected:
+    Room* m_room1;
+    Room* m_room2;
+    bool m_isOpen;
+};
+
+class Maze {
+public:
+    Maze();
+
+    void AddRoom(Room*);
+    Room* RoomNo(int) const;
+
+protected:
+    std::vector<Room*> m_Rooms;
+};
+
+class MazeGame {
+public:
+    MazeGame();
+
+    Maze* CreateMaze();
+
+protected:
+    Maze* m_Maze;
+};
+
 } // namespace DP
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include "spdlog/spdlog.h"
+#include "Creational.h"
 
-int main() {
+/*
     // 使用默认日志器
     auto console_logger = spdlog::default_logger_raw();
     spdlog::info("Welcome to spdlog!");
+*/
+
+using namespace DP;
+
+int main() {
+    MazeGame* mazeGame = new MazeGame();
+    Maze* maze = mazeGame->CreateMaze();
+
+    int num = 0;
+    while (num >= 0) {
+        std::cout << "Enter a number: ";
+        std::cin >> num;
+        std::cout << "You entered: " << num << std::endl;
+
+        Room* room = maze->RoomNo(num);
+        room->Enter();
+    }
 
     return 0;
 }
